543038dfb60309e7293384cb77b91a325e6ae154
Ricardo Meneses <ricardomeneses0503@gmail.com>
2018-11-20 21:06:02 -0600

    Terminado YA

--- backendAplicacion/DB.js
+++ backendAplicacion/DB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URL = 'mongodb://prueba:<PASSWORD>@ds125016.mlab.com:25016/curso-devf'
+const URL = 'Tu URL'
 mongoose.connect(URL,{useNewUrlParser:true},()=>{
     console.log('Conexion exitosa a la base de datos')
 })
